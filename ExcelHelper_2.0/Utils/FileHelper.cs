b5ae4bacefa0a10ad3af0e83f758dd0f85f11694
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-04-28 20:15:24 +0200

    Added FileHelper util

---
+++ ExcelHelper_2.0/Utils/FileHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace ExcelHelper_2._0.Utils
+{
+    class FileHelper
+    {
+        static FileInfo DeleteFileIfExist(FileInfo newFile)
+        {
+            if (newFile.Exists)
+            {
+                newFile.Delete();
+                newFile = new FileInfo(newFile.FullName);
+            }
+            return newFile;
+        }
+
+        static string FixFileNameExcel(string fileName)
+        {
+            if (Path.HasExtension(fileName))
+            {
+                string extension = Path.GetExtension(fileName);
+                if (extension != ".xlsx")
+                {
+                    fileName = Path.GetFileNameWithoutExtension(fileName) + ".xlsx";
+                }
+            }
+            else
+            {
+                fileName += ".xlsx";
+            }
+            return fileName;
+        }
+    }
+}
