e1e2e7c189df4a9bf2b5b67a7655e710c299eed9
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-04-28 21:32:00 +0200

    Added IExcelFile & its implementation

---
+++ ExcelHelper_2.0/ExcelFie.cs
@@ -0,0 +1,14 @@
+﻿using OfficeOpenXml;
+
+namespace ExcelHelper_2._0
+{
+    class ExcelFie : IExcelFile
+    {
+        public ExcelPackage Excel { get; }
+
+        public ExcelFie(ExcelPackage excel)
+        {
+            Excel = excel;
+        }
+    }
+}
---
+++ ExcelHelper_2.0/IExcelFile.cs
@@ -0,0 +1,9 @@
+﻿using OfficeOpenXml;
+
+namespace ExcelHelper_2._0
+{
+    public interface IExcelFile
+    {
+        ExcelPackage Excel { get; }
+    }
+}
