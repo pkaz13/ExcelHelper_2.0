ccfd62ee3aa48c2dd0548f3476441e87bf2be7f3
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-04-28 21:31:12 +0200

    Added class for EPPlus extension methods

---
+++ ExcelHelper_2.0/Utils/EPPlusExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ExcelHelper_2._0.Attributes;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ExcelHelper_2._0.Utils
+{
+    static class EPPlusExtensions
+    {
+        public static ExcelRangeBase LoadFromCollectionFiltered<T>(this ExcelRangeBase @this, IEnumerable<T> collection)
+        {
+            MemberInfo[] membersToInclude = typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public).Where(x => !Attribute.IsDefined(x, typeof(ExcelIgnore))).ToArray();
+            return @this.LoadFromCollection(collection, true, OfficeOpenXml.Table.TableStyles.Dark2, BindingFlags.Instance | BindingFlags.Public, membersToInclude);
+        }
+    }
+}
