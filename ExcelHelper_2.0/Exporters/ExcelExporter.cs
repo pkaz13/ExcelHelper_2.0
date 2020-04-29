7d03763848f4b0f36c5f8416fd96f3917604aac5
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-04-29 21:10:53 +0200

    Added IExcelExporter interface & its implementation

---
+++ ExcelHelper_2.0/Exporters/ExcelExporter.cs
@@ -0,0 +1,34 @@
+﻿using ExcelHelper_2.Utils;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExcelHelper_2.Exporters
+{
+    public class ExcelExporter : IExcelExporter
+    {
+        private const string sheetName = "Sheet1";
+
+        private readonly IExcelCreator _excelCreator;
+
+        public ExcelExporter(IExcelCreator excelCreator)
+        {
+            _excelCreator = excelCreator;
+        }
+
+        /// <summary>
+        /// Creates Excel file based on T and saves it to specified path.
+        /// </summary>
+        public void Export<T>(IEnumerable<T> collection, List<string> columnNames, string path)
+        {
+            FileInfo fileInfo = CreateFileInfo(path);
+            IExcelFile excelFile = _excelCreator.Create(collection, sheetName, columnNames);
+            excelFile.Save(fileInfo);
+        }
+
+        private FileInfo CreateFileInfo(string path)
+        {
+            FileInfo file = new FileInfo(path);
+            return FileHelper.DeleteFileIfExistAndCreateNewFile(file);
+        }
+    }
+}
---
+++ ExcelHelper_2.0/Exporters/IExcelExporter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ExcelHelper_2.Exporters
+{
+    public interface IExcelExporter
+    {
+        void Export<T>(IEnumerable<T> collection, List<string> columnNames, string path);
+    }
+}
