8e19a41fe0e8c45d10a261daafae1af4a453da52
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-04-29 22:14:49 +0200

    Added unit test project

---
+++ ExcelHelperTests.Unit/ExcelExporterTests.cs
@@ -0,0 +1,41 @@
+﻿using ExcelHelper_2;
+using ExcelHelper_2.Creators;
+using ExcelHelper_2.Exporters;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExcelHelperTests.Unit
+{
+    [TestFixture]
+    class ExcelExporterTests
+    {
+        Mock<IExcelCreator> _excelCreatorMock;
+        Mock<IExcelFile> _excelFileMock;
+        ExcelExporter _excelExporter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _excelCreatorMock = new Mock<IExcelCreator>();
+            _excelFileMock = new Mock<IExcelFile>();
+            _excelExporter = new ExcelExporter(_excelCreatorMock.Object);
+        }
+
+        [Test]
+        public void Test_Should_Export_Excel_File()
+        {
+            _excelCreatorMock.Setup(x => x.Create(It.IsAny<IEnumerable<TestExcelObject>>(), It.IsAny<string>(), It.IsAny<List<string>>())).Returns(_excelFileMock.Object);
+            List<TestExcelObject> testExcelObjects = new List<TestExcelObject>
+            {
+                new TestExcelObject("name1", 1),
+                new TestExcelObject("name2", 2)
+            };
+
+            _excelExporter.Export(testExcelObjects, new List<string> { "name", "value" }, @"test\path\file.xlsx");
+
+            _excelFileMock.Verify(x => x.Save(It.IsAny<FileInfo>()), Times.Once);
+        }
+    }
+}
---
+++ ExcelHelperTests.Unit/TestExcelObject.cs
@@ -0,0 +1,14 @@
+﻿namespace ExcelHelperTests.Unit
+{
+    class TestExcelObject
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public TestExcelObject(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
