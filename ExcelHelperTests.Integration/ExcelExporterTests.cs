e24061436973554bc3f966c8c09625fdabf65c19
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-04-29 22:44:38 +0200

    Added project for integration tests

---
+++ ExcelHelperTests.Integration/ExcelExporterTests.cs
@@ -0,0 +1,41 @@
+﻿using ExcelHelper_2.Creators;
+using ExcelHelper_2.Exporters;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ExcelHelperTests.Integration
+{
+    [TestFixture]
+    class ExcelExporterTests
+    {
+        IExcelCreator _excelCreator;
+        IExcelExporter _excelExporter;
+
+        const string path = @"C:\Users\piotr\Desktop\test.xlsx";
+        //const string path = @"\..\..\..\test.xlsx";
+
+        [SetUp]
+        public void MyTestMethod()
+        {
+            _excelCreator = new ExcelCreator();
+            _excelExporter = new ExcelExporter(_excelCreator);
+        }
+
+        [Test]
+        public void Test_Should_Export_Object_To_Excel()
+        {
+            List<TestExcelObject> testExcelObjects = new List<TestExcelObject>
+            {
+                new TestExcelObject("name1", 1),
+                new TestExcelObject("name2", 2)
+            };
+            List<string> columnNames = new List<string>
+            {
+                "name",
+                "value"
+            };
+
+            _excelExporter.Export(testExcelObjects, columnNames, path);
+        }
+    }
+}
---
+++ ExcelHelperTests.Integration/TestExcelObject.cs
@@ -0,0 +1,14 @@
+﻿namespace ExcelHelperTests.Integration
+{
+    class TestExcelObject
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public TestExcelObject(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
