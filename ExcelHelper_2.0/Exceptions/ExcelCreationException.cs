6fb193a5df9bc81a91032c269a32f6fb0199ba07
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-05-01 20:39:51 +0200

    Added custom exception

---
+++ ExcelHelper_2.0/Exceptions/ExcelCreationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExcelHelper_2._0.Exceptions
+{
+    public class ExcelCreationException : Exception
+    {
+        public ExcelCreationException() { }
+
+        public ExcelCreationException(string message) : base(message) { }
+
+        public ExcelCreationException(string message, Exception inner) : base(message, inner) { }
+    }
+}
