3832d0d28a14b54bd42adda55f56a7fbaf253a3e
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-05-02 17:22:38 +0200

    Added ExcelConvertionException

---
+++ ExcelHelper_2.0/Exceptions/ExcelConvertionException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExcelHelper_2._0.Exceptions
+{
+    public class ExcelConvertionException : Exception
+    {
+        public ExcelConvertionException() { }
+
+        public ExcelConvertionException(string message) : base(message) { }
+
+        public ExcelConvertionException(string message, Exception inner) : base(message, inner) { }
+    }
+}
