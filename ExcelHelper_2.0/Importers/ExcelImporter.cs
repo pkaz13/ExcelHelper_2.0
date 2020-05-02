c3cb82b42afba7eb4eac09a1810f2bc648f60d28
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-05-02 17:20:03 +0200

    Added IExcelImporter & its implementation
    Please enter the commit message for your changes. Lines starting

---
+++ ExcelHelper_2.0/Importers/ExcelImporter.cs
@@ -0,0 +1,34 @@
+﻿using ExcelHelper_2.Creators;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExcelHelper_2._0.Importers
+{
+    public class ExcelImporter<T> : IExcelImporter<T> where T : new()
+    {
+        private readonly IExcelCreator _excelCreator;
+
+        public ExcelImporter(IExcelCreator excelCreator)
+        {
+            _excelCreator = excelCreator;
+        }
+
+        /// <summary>
+        /// Returns list of T based on *.xlsx worksheet.
+        /// </summary>
+        public IEnumerable<T> Import(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentException("Path cannot be null or empty");
+            }
+
+            using (FileStream fileStream = new FileStream(path, FileMode.Open))
+            using (IExcelFile excelFile = _excelCreator.Create(fileStream))
+            {
+                return excelFile.ConvertToObjects<T>();
+            }
+        }
+    }
+}
---
+++ ExcelHelper_2.0/Importers/IExcelImporter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ExcelHelper_2._0.Importers
+{
+    public interface IExcelImporter<T>
+    {
+        IEnumerable<T> Import(string path);
+    }
+}
