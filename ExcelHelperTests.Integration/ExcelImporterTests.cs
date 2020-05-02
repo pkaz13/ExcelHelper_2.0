fbb33dd4f482aac9063d9fee56bdbba43f4e1bad
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-05-02 19:24:39 +0200

    Added ExcelImporter integration tests

---
+++ ExcelHelperTests.Integration/ExcelImporterTests.cs
@@ -0,0 +1,35 @@
+﻿using ExcelHelper_2._0.Importers;
+using ExcelHelper_2._0.Wrappers;
+using ExcelHelper_2.Creators;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExcelHelperTests.Integration
+{
+    [TestFixture]
+    class ExcelImporterTests
+    {
+        IExcelCreator _excelCreator;
+        IFileStreamWrapper _fileStreamWrapper;
+        IExcelImporter<TestExcelObject> _excelImporter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _excelCreator = new ExcelCreator();
+            _fileStreamWrapper = new FileStreamWrapper();
+            _excelImporter = new ExcelImporter<TestExcelObject>(_excelCreator, _fileStreamWrapper);
+        }
+
+        [Test]
+        public void Test_Should_Import_File()
+        {
+            string path = @"..\..\..\..\test.xlsx";
+
+            IEnumerable<TestExcelObject> result = _excelImporter.Import(path).ToList();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
--- ExcelHelperTests.Integration/TestExcelObject.cs
+++ ExcelHelperTests.Integration/TestExcelObject.cs
@@ -1,14 +1,23 @@
-﻿namespace ExcelHelperTests.Integration
+﻿using ExcelHelper_2.Attributes;
+
+namespace ExcelHelperTests.Integration
 {
     class TestExcelObject
     {
+        [ExcelColumn(1)]
         public string Name { get; set; }
+        [ExcelColumn(2)]
         public int Value { get; set; }
 
         public TestExcelObject(string name, int value)
         {
             Name = name;
             Value = value;
+        }
+
+        public TestExcelObject()
+        {
+
         }
     }
 }
