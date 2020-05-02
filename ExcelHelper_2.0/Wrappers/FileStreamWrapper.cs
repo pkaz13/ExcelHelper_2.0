b8d98d7601f24da4e3aa572fbdfb2162b9c28c83
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-05-02 18:51:19 +0200

    Added file stream wrapper

---
+++ ExcelHelper_2.0/Wrappers/FileStreamWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace ExcelHelper_2._0.Wrappers
+{
+    class FileStreamWrapper : IFileStreamWrapper
+    {
+        public FileStream Init(string path, FileMode fileMode)
+        {
+            return new FileStream(path, fileMode);
+        }
+    }
+}
---
+++ ExcelHelper_2.0/Wrappers/IFileStreamWrapper.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace ExcelHelper_2._0.Wrappers
+{
+    public interface IFileStreamWrapper
+    {
+        FileStream Init(string path, FileMode fileMode);
+    }
+}
